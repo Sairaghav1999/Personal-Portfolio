20ef1822ba30c3c940aba9d57f914035d436b170
Sairaghav1999 <viravalli.s@northeastern.edu>
2022-01-20 09:59:00 -0500

    Update contact mail

--- src/components/Contact/EmailLink.js
+++ src/components/Contact/EmailLink.js
@@ -76,9 +76,9 @@ const EmailLink = () => {
       onMouseEnter={() => setIsActive(false)}
       onMouseLeave={() => (idx < messages.length) && setIsActive(true)}
     >
-      <a href={validateText(message) ? `mailto:${message}@mldangelo.com` : ''}>
-        <span>{message}</span>
-        <span>@mldangelo.com</span>
+      <a href={validateText(message) ? `mailto:vsai.raghavendra1999@gmail.com` : ''}>
+       
+        <span>vsai.raghavendra1999@gmail.com</span>
       </a>
     </div>
   );
