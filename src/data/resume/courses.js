1a5e9f956fbf24c90f93952d048acb1391fdadb0
Sairaghav1999 <viravalli.s@northeastern.edu>
2022-06-05 22:58:37 +0530

    modify course

--- src/data/resume/courses.js
+++ src/data/resume/courses.js
@@ -11,6 +11,12 @@ const courses = [
     link: 'https://wl11gp.neu.edu/udcprod8/bwckctlg.p_disp_course_detail?cat_term_in=202030&subj_code_in=INFO&crse_numb_in=6105',
     university: 'Northeastern University',
   },
+  {
+    title: 'Application Engineering and Development',
+    number: 'INFO 5100',
+    link: 'http://catalog.northeastern.edu/course-descriptions/info/',
+    university: 'Northeastern University',
+  },
 ];
 
 export default courses;
